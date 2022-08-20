688cb54bed0bf2d9a5185138c527f8aa094f2414
ChengwenLi0506 <1102016069@qq.com>
2022-08-21 01:35:15 +0800

    One problem

--- Src/python_2/sales_trend_chart.py
+++ Src/python_2/sales_trend_chart.py
@@ -1,3 +1,5 @@
+from pyecharts.charts import *
+from pyecharts.commons.utils import JsCode
 from pyecharts import options as opts
 from pyecharts.charts import Bar, Grid, Line
 import pandas as pd
@@ -5,35 +7,54 @@ import pandas as pd
 df = pd.read_excel("byd_sales.xlsx", sheet_name= '2021')
 df2022 = pd.read_excel("byd_sales.xlsx", sheet_name= '2022')
 print(df)
+
 #2021 sheet
 data = df.values
+
 #2022sheet
 data2 = df2022.values
+
 #2021 销量列
 sell21 = df['销量'].values.tolist()
+
 #2022 销量列
 sell22 = df2022['销量'].values.tolist()
+
 #同比增长率
 linedata = ((df2022['销量'].values - df['销量'].values) / df['销量'].values * 100).tolist()
+#for i in range(0, len(linedata)):
+    #linedata[i] = "%.1f%%"%(linedata[i]),
+#print(linedata,type(linedata))
+
 #x轴
 x_data = ["{}月".format(i) for i in range(1, 8)]
-
 bar = (
     Bar()
-    .set_global_opts(title_opts=opts.TitleOpts(title="2021-2022 年销量数据同比"))
+    .set_global_opts(
+        title_opts=opts.TitleOpts(title="2021-2022 年销量数据同比", pos_left='center',title_textstyle_opts=opts.TextStyleOpts(color='#FFFFFF',font_weight='bold', font_size=30)),
+        legend_opts=opts.LegendOpts(pos_top='bottom'),
+        xaxis_opts=opts.AxisOpts(axistick_opts=opts.AxisTickOpts(is_show=False),axisline_opts=opts.AxisLineOpts(linestyle_opts=opts.LineStyleOpts(color='#FFFFFF'))),
+        yaxis_opts=opts.AxisOpts(
+            is_show=False,
+            axisline_opts=opts.AxisLineOpts(is_show=False),
+            axistick_opts=opts.AxisTickOpts(is_show=False)
+        )
+    )
     .add_xaxis(x_data)
     #每年销量柱状图
     .add_yaxis(
         "2021年",
         sell21,
         yaxis_index=0,
-        color="	#5793f3",
+        color="#5793f3",
+        label_opts=opts.LabelOpts(is_show=True,color='#FFFFFF'),
     )
     .add_yaxis(
         "2022年",
         sell22,
         yaxis_index=0,
         color="#87CEEB",
+        label_opts=opts.LabelOpts(is_show=True,color='#FFFFFF'),
     )
     #右边刻度
     .extend_axis(
@@ -43,10 +64,9 @@ bar = (
             max_=350,
             position="right",
             split_number = 7,
-            axisline_opts=opts.AxisLineOpts(
-                linestyle_opts=opts.LineStyleOpts(color="#d14a61")
-            ),
-            axislabel_opts=opts.LabelOpts(formatter="{value} %"),
+            axisline_opts=opts.AxisLineOpts(is_show=False),
+            axistick_opts=opts.AxisTickOpts(is_show=False),
+            axislabel_opts=opts.LabelOpts(color='#FFFFFF',formatter="{value}%"),
         )
     )
     #左边刻度
@@ -58,6 +78,10 @@ bar = (
             grid_index = 0,
             split_number = 9,
             interval = 20000,
+            axisline_opts=opts.AxisLineOpts(is_show=False),
+            axistick_opts=opts.AxisTickOpts(is_show=False),
+            axislabel_opts=opts.LabelOpts(color='#FFFFFF'),
+            splitline_opts=opts.SplitLineOpts(is_show=True),
         )
     )
 )
@@ -68,13 +92,14 @@ line = (
     .add_yaxis(
         "同比增长率",
         linedata,
+        linestyle_opts=opts.LineStyleOpts(color='#87CEFA'),
         yaxis_index=1,
-        color="#5793f3",
-        label_opts=opts.LabelOpts(is_show=False),
+        label_opts=opts.LabelOpts(color='#FFFFFF'),
     )
 )
 
 bar.overlap(line)
 grid = Grid()
-grid.add(bar, opts.GridOpts(pos_left="5%", pos_right="20%"), is_control_axis_index=True)
+grid.bg_color = "#808080"
+grid.add(bar, opts.GridOpts(pos_left="20%", pos_right="20%"), is_control_axis_index=True)
 grid.render("grid_multi_yaxis.html")
